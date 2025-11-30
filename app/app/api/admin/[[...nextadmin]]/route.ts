adae1c281e5d0960d617bd59c5aedc823be4c7a5
toto-inu <oztodog@gmail.com>
2025-12-01 00:37:11 +0900

    feat: add Next-Admin API route handler

---
+++ app/app/api/admin/[[...nextadmin]]/route.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@/lib/prisma";
+import { createHandler } from "@premieroctet/next-admin/dist/appHandler";
+
+const { run } = createHandler({
+  apiBasePath: "/api/admin",
+  prisma,
+});
+
+export { run as DELETE, run as GET, run as POST };
