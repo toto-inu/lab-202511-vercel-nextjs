947663cb3a9762655c3a0224860b8d8f4faef877
toto-inu <oztodog@gmail.com>
2025-12-01 00:38:01 +0900

    docs: add authentication warning to admin page
    Note: Authentication should be implemented in the future using Clerk with ADMIN role check

--- app/app/admin/[[...nextadmin]]/page.tsx
+++ app/app/admin/[[...nextadmin]]/page.tsx
@@ -4,6 +4,14 @@ import { prisma } from "@/lib/prisma";
 import schema from "@/prisma/json-schema/json-schema.json";
 import "@/app/globals.css";
 
+/**
+ * Next-Admin Dashboard Page
+ *
+ * WARNING: This admin interface is currently NOT protected by authentication.
+ * In production, you should add authentication middleware to protect this route.
+ *
+ * TODO: Add Clerk authentication with ADMIN role check
+ */
 export default async function AdminPage({
   params,
   searchParams,
